26f30ec1d54c7c2b90566cd38a82ce15737cb538
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-18 10:24:53 +0300

    [Lib/feat] Создал класс `PointUtilities` с методами для точки

---
+++ Lib/PointUtilities.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+
+namespace Lib;
+
+public static class PointUtilities
+{
+	public static Point Random(uint dimension) =>
+		new(
+			Utilities.Generate(
+				dimension,
+				() => System.Random.Shared.NextDouble() * 100
+			)
+		);
+}
