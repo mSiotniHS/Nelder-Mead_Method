a0bc249244a80b6121bd69e83529c40e1a06cc0d
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-18 10:30:34 +0300

    [App/feat] Расписал пример работы программы

---
+++ App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Lib;
+using Lib.Functions;
+
+namespace App;
+
+public static class Program
+{
+	public static void Main()
+	{
+		var rosenbrock = Rosenbrock.Classic();
+		var classic = new Coefficients
+		{
+			Reflection = 1,
+			Expansion = 2,
+			Shrink = 0.5
+		};
+
+		var method = new NelderMeadMethod(classic, NoMoreThanNIterations);
+		var min = method.FindMinimum(rosenbrock);
+		Console.WriteLine(min);
+	}
+
+	private static bool NoMoreThanNIterations(Statistics<Simplex> statistics)
+		=> statistics.IterationCount < 60;
+}
