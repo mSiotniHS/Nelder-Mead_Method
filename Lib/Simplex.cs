f2fdd3e1e2be402311608d3443bb57e211e6fe19
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-18 10:26:24 +0300

    [Lib/feat] Определил класс `Simplex` для хранения симплекса

---
+++ Lib/Simplex.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lib;
+
+public sealed class Simplex
+{
+	private readonly Point[] _simplex;
+	public uint Size => (uint) _simplex.Length;
+
+	public Simplex(IEnumerable<Point> simplex)
+	{
+		_simplex = simplex.ToArray();
+	}
+
+	public Point this[Index idx]
+	{
+		get => _simplex[idx];
+		set => _simplex[idx] = value;
+	}
+
+	public Simplex Replace(Point replaced, Point added)
+	{
+		var newSimplex = _simplex.ToArray();
+		var idx = Array.IndexOf(newSimplex, replaced);
+
+		if (idx == -1)
+		{
+			throw new ArgumentException("Point is not in simplex", nameof(replaced));
+		}
+
+		newSimplex[idx] = added;
+
+		return new Simplex(newSimplex);
+	}
+
+	public Simplex Map(Func<Point, Point> map)
+		=> new(_simplex.Select(map));
+
+	public Point Centroid(Point except)
+	{
+		var centroid = Point.Zero(_simplex[0].Dimension);
+		var count = 0;
+
+		foreach (var point in _simplex)
+		{
+			if (point.Equals(except)) continue;
+
+			centroid += point;
+			count++;
+		}
+
+		return centroid / count;
+	}
+}
