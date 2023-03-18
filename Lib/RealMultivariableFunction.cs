2cf1ccc070aad0a84edcd0e90d9110e119f3b148
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-18 10:25:40 +0300

    [Lib/feat] Определил абстрактный класс `RealMultivariableFunction`

---
+++ Lib/RealMultivariableFunction.cs
@@ -0,0 +1,14 @@
+﻿namespace Lib;
+
+public abstract class RealMultivariableFunction
+{
+	public abstract uint Dimension { get; }
+
+	protected abstract double BaseCalculate(Point point);
+
+	public double Calculate(Point point)
+	{
+		point.DimensionAssert(Dimension);
+		return BaseCalculate(point);
+	}
+}
