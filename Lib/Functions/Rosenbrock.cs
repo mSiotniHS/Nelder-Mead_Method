4e3576a639afb477c551a6cba57d79996abcef47
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-18 10:30:16 +0300

    [Lib/feat] Задал тестовую функцию Розенброка

---
+++ Lib/Functions/Rosenbrock.cs
@@ -0,0 +1,27 @@
+﻿using static System.Math;
+
+namespace Lib.Functions;
+
+public sealed class Rosenbrock : RealMultivariableFunction
+{
+	private readonly double _a;
+	private readonly double _b;
+
+	public Rosenbrock(double a, double b)
+	{
+		_a = a;
+		_b = b;
+	}
+
+	public static Rosenbrock Classic() => new(1, 100);
+
+	public override uint Dimension => 2;
+
+	protected override double BaseCalculate(Point point)
+	{
+		var x = point[0];
+		var y = point[1];
+
+		return Pow(_a - x, 2) + _b * Pow(y - Pow(x, 2), 2);
+	}
+}
