e61263d84ee130d4cc31023480e26c315e63a0a1
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-18 10:28:43 +0300

    [Lib/feat] Создал класс `Statistics` для хранения симплексов в процессе работы алгоритма

---
+++ Lib/Statistics.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Lib;
+
+public sealed class Statistics<T>
+{
+	public List<T> Trace { get; }
+	public uint IterationCount { get; private set; }
+
+	private readonly uint _savingFrequency;
+	private readonly uint _maxTraceLength;
+
+	private uint _counter;
+
+	public Statistics(uint savingFrequency, uint maxTraceLength)
+	{
+		Trace = new List<T>();
+
+		_savingFrequency = savingFrequency;
+		_maxTraceLength = maxTraceLength;
+
+		_counter = 0;
+		IterationCount = 0;
+	}
+
+	public static Statistics<T> Classic() =>
+		new(1, uint.MaxValue);
+
+	public void Save(T thing)
+	{
+		IterationCount++;
+
+		_counter++;
+		if (_counter != _savingFrequency) return;
+
+		Trace.Add(thing);
+		if (Trace.Count > _maxTraceLength) Trace.RemoveAt(0);
+		_counter = 0;
+	}
+}
