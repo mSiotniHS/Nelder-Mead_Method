b34604783418737e7af9a6f8bd1b856ecf88b94b
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-19 18:05:14 +0300

    [Lib.Tests] Начал писать тесты для `Simplex`

---
+++ Lib.Tests/SimplexTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Lib.Tests;
+
+public static class SimplexTests
+{
+	public sealed class ReplaceMethod
+	{
+		[Fact]
+		public void ReplaceShouldGenerateCorrectSimplex()
+		{
+			var added = new Point(0, 0);
+
+			var simplex = new Simplex(new List<Point> {new(1, 2), new(3, 4)});
+			var modifiedSimplex = simplex.Replace(new(1, 2), added);
+
+			Assert.Contains(added, modifiedSimplex);
+			Assert.DoesNotContain(new Point(1, 2), modifiedSimplex);
+		}
+
+		[Fact]
+		public void ReplaceShouldNotModifyInitialSimplex()
+		{
+			var added = new Point(0, 0);
+
+			var simplex = new Simplex(new List<Point> {new(1, 2), new(3, 4)});
+			var _ = simplex.Replace(new(1, 2), added);
+
+			Assert.Contains(new(1, 2), simplex);
+			Assert.DoesNotContain(new Point(0, 0), simplex);
+		}
+	}
+}
