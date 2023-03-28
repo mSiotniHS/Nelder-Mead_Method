5e8e98c97e5952b34ed6587f82ec979d77d39cb8
Artyom Abramyan <artemabramyan7@icloud.com>
2023-03-28 20:08:00 +0300

    [Lib/feat] Добавил `FileLogger`

---
+++ Lib/Helpers/FileLogger.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Lib.Helpers;
+
+public sealed class FileLogger : ILogger
+{
+	private readonly StreamWriter _writer;
+
+	public FileLogger(StreamWriter writer)
+	{
+		_writer = writer;
+	}
+
+	public void Log(string log) => _writer.WriteLine(log);
+}
