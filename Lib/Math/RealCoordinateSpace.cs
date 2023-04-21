9c77bf962625c8b4935c3af10289b56459a8419c
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-21 14:05:50 +0300

    [Lib/feat] Добавил возможность указания размерности в `RealCoordinateSpace`

--- Lib/Math/RealCoordinateSpace.cs
+++ Lib/Math/RealCoordinateSpace.cs
@@ -4,5 +4,12 @@ namespace Lib.Math;
 
 public class RealCoordinateSpace : ISet<Point>
 {
-	public virtual bool Has(Point point) => point.All(double.IsRealNumber);
+	public uint Dimension { get; init; }
+
+	public RealCoordinateSpace(uint dimension)
+	{
+		Dimension = dimension;
+	}
+
+	public virtual bool Has(Point point) => point.Dimension == Dimension && point.All(double.IsRealNumber);
 }
