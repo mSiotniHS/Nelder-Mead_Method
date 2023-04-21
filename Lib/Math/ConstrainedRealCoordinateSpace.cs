daaaf6f7dee2dfef82f0fe021e0a0459bdae405b
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-21 14:06:34 +0300

    [Lib/feat] Добавил класс, представляющий собой \R^n с ограничениями

---
+++ Lib/Math/ConstrainedRealCoordinateSpace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Lib.Math;
+
+public class ConstrainedRealCoordinateSpace : RealCoordinateSpace
+{
+	/// <summary>
+	/// Возвращает <c>true</c>, если точка удовлетворяет ограничению; иначе <c>false</c>
+	/// </summary>
+	public delegate bool Constraint(Point point);
+
+	private readonly Constraint[] _constraints;
+
+	public ConstrainedRealCoordinateSpace(uint dimension, IEnumerable<Constraint> constraints) : base(dimension)
+	{
+		_constraints = constraints.ToArray();
+	}
+
+	public override bool Has(Point point)
+	{
+		return
+			base.Has(point)
+			&& _constraints.All(constraint => constraint(point));
+	}
+}
